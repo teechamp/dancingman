543fee55835c6b77728ec3d796a9af10d7731d10
teechamp <temidayosamuelbambo@gmail.com>
2020-10-13 15:54:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dancing_man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("         ___");
+            Console.WriteLine("    \\\\__|__|__ ");
+            Console.WriteLine("      \\( o_o)");
+            Console.WriteLine("        > ~ >");
+            Console.WriteLine("       /   / \\\\ ");
+            Console.WriteLine("      /   /   \\\\ ");
+            Console.WriteLine("      \\ )     c/ ");
+            Console.WriteLine("    /   /  ");
+            Console.WriteLine("   /  /|   ");
+            Console.WriteLine ("  ( (  \\   ");
+            Console.WriteLine ("  | | \\  ");
+            Console.WriteLine ("  | / \\ )  ");
+            Console.WriteLine ("  | | ) |  ");
+            Console.WriteLine (" / )(_/  ");
+            Console.WriteLine ("(_ / ");
+            Console.WriteLine ("- Dancing Man ");
+ 
+        }
+    }
+}
